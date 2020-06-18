8afeea21d4f0604a29800677c7e2f2c58e066df6
sgouezel <sebastien.gouezel@univ-rennes1.fr>
2020-06-18 18:31:27 +0200

    merge master

--- src/geometry/manifold/charted_space.lean
+++ src/geometry/manifold/charted_space.lean
@@ -550,20 +550,26 @@ that changing coordinates with an atlas member gives an element of the groupoid.
 def structure_groupoid.maximal_atlas : set (local_homeomorph M H) :=
 {e | ∀ e' ∈ atlas H M, e.symm ≫ₕ e' ∈ G ∧ e'.symm ≫ₕ e ∈ G}
 
-variables {M G}
+variable {M}
+
+/-- The elements of the atlas belong to the maximal atlas for any structure groupoid -/
+lemma structure_groupoid.mem_maximal_atlas_of_mem_atlas [has_groupoid M G]
+  {e : local_homeomorph M H} (he : e ∈ atlas H M) : e ∈ G.maximal_atlas M :=
+λ e' he', ⟨G.compatible he he', G.compatible he' he⟩
+
+lemma structure_groupoid.chart_mem_maximal_atlas [has_groupoid M G]
+  (x : M) : chart_at H x ∈ G.maximal_atlas M :=
+G.mem_maximal_atlas_of_mem_atlas (chart_mem_atlas H x)
+
+variable {G}
 
 lemma mem_maximal_atlas_iff {e : local_homeomorph M H} :
   e ∈ G.maximal_atlas M ↔ ∀ e' ∈ atlas H M, e.symm ≫ₕ e' ∈ G ∧ e'.symm ≫ₕ e ∈ G :=
 iff.rfl
 
-/-- The elements of the atlas belong to the maximal atlas for any structure groupoid -/
-lemma mem_maximal_atlas_of_mem_atlas [has_groupoid M G]
-  {e : local_homeomorph M H} (he : e ∈ atlas H M) : e ∈ G.maximal_atlas M :=
-λ e' he', ⟨G.compatible he he', G.compatible he' he⟩
-
 /-- Changing coordinates between two elements of the maximal atlas gives rise to an element
 of the structure groupoid. -/
-lemma structure_groupoid.compatible_of_mem_maximal_atlas {e e' : local_homeomorph M H}
+lemma compatible_of_mem_maximal_atlas {e e' : local_homeomorph M H}
   (he : e ∈ G.maximal_atlas M) (he' : e' ∈ G.maximal_atlas M) :
   e.symm ≫ₕ e' ∈ G :=
 begin
